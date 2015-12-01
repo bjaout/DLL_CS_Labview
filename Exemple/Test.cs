4c35d43a929f105ab27520923511b1d76d4401bf
bjaout <benjamin.mestre@gmail.com>
2015-12-01 13:52:33 +0100

    Exemple de dll

---
+++ Exemple/Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple
+{
+    public class Test
+    {
+        /// <summary>
+        /// Déclaration d'un compteur pour avoir quelque chose de visuel
+        /// </summary>
+        private int counter = 0;
+        public int Counter
+        {
+            get { return counter; }
+        }
+
+        public delegate void TestHandler(object myObj, TestArgs myArgs); // On déclare un délégé de fonction (un type de fonction)
+        public event TestHandler TestEvent; // On déclare un événement personalisé dont la fonction de gestion doit respecter le délégué utilisé dans la définition
+
+        /// <summary>
+        /// Déclaration d'un constructeur initialisant le compteur à 0
+        /// </summary>
+        public Test()
+        {
+            counter = 0;
+        }
+
+        /// <summary>
+        /// Incrémente le compteur de 1 puis déclenche l'événement TestEvent qui peut ou non être géré
+        /// </summary>
+        public void IncrementCounter()
+        {
+            counter++;
+            TestArgs myArgs = new TestArgs(counter.ToString());
+            TestEvent(this, myArgs);
+        }
+    }
+}
---
+++ Exemple/TestArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exemple
+{
+    /// <summary>
+    /// Création d'une classe spécifique pour nos arguments d'événements
+    /// </summary>
+    public class TestArgs:EventArgs
+    {
+        /// <summary>
+        /// Un message qui pourra être récupéré
+        /// </summary>
+        private string message;
+        public string Message
+        {
+            get { return message; }
+        }
+
+        /// <summary>
+        /// Constructeur de la classe on lui passe le message à stocker qui sera ensuite accessible par le gestionnaire d'événement
+        /// </summary>
+        /// <param name="msg">Le message à stocker</param>
+        public TestArgs(string msg)
+        {
+            this.message = msg;
+        }
+
+    }
+}
